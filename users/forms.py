336fbcf4b273a8654f5175f8a907ba1553c66211
sbtiffanykim <sbtiffanykim@gmail.com>
2020-08-21 15:40:47 +0900

    Working on SignUp Form

--- users/forms.py
+++ users/forms.py
@@ -18,3 +18,42 @@ class LoginForm(forms.Form):
                 self.add_error("password", forms.ValidationError("Password is wrong"))
         except models.User.DoesNotExist:
             self.add_error("email", forms.ValidationError("User Does Not Exist"))
+
+
+class SignUpForm(forms.Form):
+
+    first_name = forms.CharField(max_length=80)
+    last_name = forms.CharField(max_length=80)
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    c_password = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get(email=email)
+            raise forms.ValidationError("User already exists with that email")
+        except models.User.DoesNotExist:
+            return email
+
+    def clean_c_password(self):
+        password = self.cleaned_data.get("password")
+        c_password = self.cleaned_data.get("c_password")
+
+        if password != c_password:
+            raise forms.ValidationError("Password confirmation does not match")
+        else:
+            return password
+
+    def save(self):
+        first_name = self.cleaned_data.get("first_name")
+        last_name = self.cleaned_data.get("last_name")
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+
+        user = models.User.objects.create_user(email, email, password)
+
+        user.first_name = first_name
+        user.last_name = last_name
+
+        user.save()
--- users/views.py
+++ users/views.py
@@ -24,3 +24,19 @@ class LoginView(FormView):
 def log_out(request):
     logout(request)
     return redirect(reverse("core:home"))
+
+
+class SignUpView(FormView):
+
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
